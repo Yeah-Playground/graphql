e001e31327c1801a07ca6d33968941236c1a6ee9
YEAHx4 <developerstarlight@gmail.com>
2022-05-03 20:38:09 +0900

    init style

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/:id" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
